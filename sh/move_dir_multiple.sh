9e26dbe9667698978faecb555a3944e25d9d54a5
Katie Ciurleo <katieciurleo@gmail.com>
2024-06-21 10:05:21 -0400

    skip copying on last commit

--- sh/move_dir_multiple.sh
+++ sh/move_dir_multiple.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Used for copying directory between old and new spots, given an input_file with a list of obsids
-# This version accounts for the possibility that the file is in two places
+# This version accounts for the possibility that the file is in two places, and skips the copying
+# if the file already exists at the end directory.
+
 input_file="/Users/kciurleo/Documents/kciurleo/AGN/csvs/haves.txt"
 
 # Directories
@@ -16,13 +18,17 @@ directory_exists() {
 
 # Copy entire directories
 while read F  ; do
-    if directory_exists "$start_dir1$F"; then
-        cp -r "$start_dir1$F" "$end_dir"
-        echo "Copied $F from $start_dir1"
-    elif directory_exists "$start_dir2$F"; then
-        cp -r "$start_dir2$F" "$end_dir"
-        echo "Copied $F from $start_dir2"
+    if directory_exists "$end_dir/$F"; then
+        echo "$F already exists in $end_dir, skipping."
     else
-        echo "Directory $F not found in either $start_dir1 or $start_dir2"
+        if directory_exists "$start_dir1$F"; then
+            cp -r "$start_dir1$F" "$end_dir"
+            echo "Copied $F from $start_dir1"
+        elif directory_exists "$start_dir2$F"; then
+            cp -r "$start_dir2$F" "$end_dir"
+            echo "Copied $F from $start_dir2"
+        else
+            echo "Directory $F not found in either $start_dir1 or $start_dir2"
+        fi
     fi
 done < "$input_file"
