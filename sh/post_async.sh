cd84f3e83b453ebb4f6813021ecce42d3585079d
Katie Ciurleo <katieciurleo@gmail.com>
2024-06-24 12:41:41 -0400

    attempting and failing to get eROSITA data

--- sh/post_async.sh
+++ sh/post_async.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-# This script runs an asynchronous post request to ESAC based on a query txt file and input votable
+# This script runs an asynchronous post request based on a query txt file, input votable, and API endpoint. 
+# Used for both XMM and eROSITA query
 
 # Files
-query_file="/Users/kciurleo/Documents/kciurleo/AGN/queries/XMM_query.txt"
+#query_file="/Users/kciurleo/Documents/kciurleo/AGN/queries/XMM_query.txt"
+query_file="/Users/kciurleo/Documents/kciurleo/AGN/queries/eROSITA_query.txt"
+#endpoint="https://nxsa.esac.esa.int/tap-server/tap/async"
+endpoint="https://heasarc.gsfc.nasa.gov/xamin/vo/tap/async"
 table="/Users/kciurleo/Documents/kciurleo/AGN/csvs/seyferts.vot"
 
 # Read the query from query.txt, changing new lines and tabs to spaces
 query=$(tr -s '[:space:]' ' ' < "$query_file")
 
 # Execute the curl command
-curl -i -X POST \
+curl -i -X POST "$endpoint"\
     --form PHASE=run \
     --form UPLOAD="table_f,param:table2" \
     --form table2=@"$table" \
@@ -18,7 +22,6 @@ curl -i -X POST \
     --form FORMAT=votable \
     --form REQUEST=doQuery \
     --form QUERY="\"$query\"" \
-    "https://nxsa.esac.esa.int/tap-server/tap/async"
 
 # After running this script, the request lists a location 
 # (e.g. Location: http://nxsa.esac.esa.int/tap-server/tap/async/1718984677210O)
