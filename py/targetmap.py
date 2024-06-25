6e0fb60097c51473c60f451cf3b1cbbccda1e4b2
Katie Ciurleo <katieciurleo@gmail.com>
2024-06-25 16:38:18 -0400

    made updated map

--- py/targetmap.py
+++ py/targetmap.py
@@ -21,22 +21,54 @@ agostino=seyferts.loc[seyferts['sl_class1']==1]
 #Specify subsections of ra and dec desired
 ras=[data['ra'], erosita['ra_cone'], XMM_result['ra'], seyferts['ra_x'], portsmouth['ra_x']]
 decs=[data['dec'],  erosita['dec_cone'], XMM_result['dec'], seyferts['dec_x'],portsmouth['dec_x']]
-colors=['silver', 'gold', 'green', 'blue', 'red']
+colors=['dimgrey', 'yellow', 'deepskyblue', 'lime', 'red']
 labels=['CSC2.1', 'eROSITA (Main) Sources', 'XMM Sources','Agostino Seyfert AGN Candidates','Portsmouth Seyfert AGN Candidates']
-alphas=[0.5,1,1, 1, 1]
+alphas=[0.15,1,1, 1, 1]
 markers=['o','o','o','x','x']
 sizes=[4,20,20,20,20]
 
+#Meridian at SagA*, where eROSITA cuts off
+lon_meridian = 359.94423568 * u.deg
+lat_array = np.linspace(-90, 90, 2000) * u.deg  # Latitude array from -90 to 90 degrees
+
+#Circle array in galactic coords
+galactic_coords = SkyCoord(l=lon_meridian, b=lat_array, frame='galactic')
+#Converted to equatorial coords
+equatorial_coords = galactic_coords.transform_to('fk5')
+
+#RA/dec of circle
+RA = equatorial_coords.ra.degree
+Dec = equatorial_coords.dec.degree
+
 #Make plot
 fig = plt.figure(figsize=(16,10))
+plt.style.use('dark_background')
 ax = fig.add_subplot(111, projection="aitoff")
+
+#eROSITA
+ax.plot(np.radians(RA-180), np.radians(Dec), '.',markersize=1, color='yellow')
+ax.plot(np.radians(RA-360), -np.radians(Dec), '.',markersize=1, color='yellow', label='eROSITA Boundary')
+#weird connecting line
+ax.plot([np.radians(RA[-1]-180), np.radians(RA[150]-360)],[np.radians(Dec[-1]), -np.radians(Dec[150])],color='yellow', linestyle='-')
+
+
 for i in range(len(ras)):
-    ax.scatter(np.radians(-(((ras[i]) % 360) - 180)), np.radians(decs[i]),marker=markers[i],c=colors[i], s=sizes[i], label=labels[i], alpha=alphas[i], linewidths=0.5)
+    ax.scatter(np.radians(ras[i]-180), np.radians(decs[i]),marker=markers[i],c=colors[i], s=sizes[i], label=labels[i], alpha=alphas[i], linewidths=1)
+#plot twice because it doesn't like to do that
+for i in range(len(ras)):
+    ax.scatter(np.radians(ras[i]-360-180), np.radians(decs[i]),marker=markers[i],c=colors[i], s=sizes[i], alpha=alphas[i], linewidths=0.5)
+
 plt.xticks(ticks=np.radians([-150, -120, -90, -60, -30, 0, \
                              30, 60, 90, 120, 150]),
-           labels=['150°', '120°', '90°', '60°', '30°', '0°', \
-                   '330°', '300°', '270°', '240°', '210°'])
+           labels=['30°', '60°', '90°', '120°', '150°', '180°', \
+                   '210°', '240°', '270°', '300°', '330°'])
+
+ax.yaxis.set_tick_params(which='both', colors='black')
+for label in ax.yaxis.get_ticklabels():
+    label.set_color('black')
+
 plt.grid()
 plt.legend(loc="upper right")
+
 plt.savefig("/Users/kciurleo/Documents/kciurleo/AGN/plots/target_map.pdf", format="pdf")
 plt.show()
