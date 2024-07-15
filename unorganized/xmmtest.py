6e83f105381640e0c8b6de9f38f8ae679f7941a7
Katie Ciurleo <katieciurleo@gmail.com>
2024-07-15 12:28:00 -0400

    make images of the twos and threes problem children

--- unorganized/xmmtest.py
+++ unorganized/xmmtest.py
@@ -1,7 +1,10 @@
 import pandas as pd
-from astropy.io import votable
-from astropy.table import Table
+from astropy.visualization import simple_norm
+from astropy.coordinates import SkyCoord
+from astroquery.skyview import SkyView
 import matplotlib.pyplot as plt
+import astropy.units as u
+import numpy as np
 '''
 XMM_result = votable.parse_single_table('/Users/kciurleo/Documents/kciurleo/AGN/csvs/XMM_query_result.vot').to_table().to_pandas()
 XMM_result.rename(columns={'ra_2':'cscra', 'dec_2':'cscdec'}, inplace=True)
@@ -32,3 +35,88 @@ print(f"Number of XMM objects with 1 unique SDSS within 15 arcsec: {len(ones['ra
 print(f"Number of XMM objects with 2 unique SDSS within 15 arcsec: {len(twos['ra'])/2}")
 print(f"Number of XMM objects with 3 unique SDSS within 15 arcsec: {len(threes['ra'])/3}")
 print(f"{(15+2)/290*100:.2e} percent are problem children")
+
+def pixel_coords(ra, dec, centerra, centerdec, deg, pix):
+    x = pix/2-(ra-centerra)/(deg/pix)
+    y = pix/2+(dec-centerdec)/(deg/pix)
+    return x, y
+
+def eq_coords(x, y, centerra, centerdec, deg, pix, whattoreturn='both'):
+    ra = centerra - (x - pix/2) * (deg / pix)
+    dec = centerdec + (y - pix/2) * (deg / pix)
+
+    if whattoreturn == 'both':
+        return ra, dec
+    elif whattoreturn =='ra':
+        return ra
+    else:
+        return dec
+
+def get_image(ra, dec, deg, pix, title):
+
+    try:
+        result = SkyView.get_images(position=SkyCoord(ra=ra[0], dec=dec[0], unit="deg"),
+                                    survey='SDSSr',
+                                    coordinates='J2000',
+                                    pixels=(pix,pix),
+                                    height=deg *u.deg,
+                                    width=deg*u.deg)
+        survey='SDSSr'
+    except:
+        result = SkyView.get_images(position=SkyCoord(ra=ra[0], dec=dec[0], unit="deg"),
+                                    survey='DSS',
+                                    coordinates='J2000',
+                                    pixels=(pix,pix),
+                                    height=deg *u.deg,
+                                    width=deg*u.deg)
+        survey='DSS'
+
+
+    image_data = result[0][0].data
+
+    # Display the image
+    plt.figure(figsize=(8, 8))
+    plt.imshow(image_data, cmap='viridis', origin='lower', norm=simple_norm(image_data, 'sqrt'))
+    plt.colorbar(label='Intensity')
+    colors = ['black','red','blue']
+    for i in range(len(ra)):
+        newra, newdec = pixel_coords(ra[i], dec[i], ra[0], dec[0], deg, pix)
+        plt.scatter(newra, newdec, marker='o', s=100, edgecolor=colors[i], facecolor='none', label=f'SDSS ({ra[i]}, {dec[i]})', alpha=0.75)
+        xmmra, xmmdec = pixel_coords(title[0], title[1], ra[0], dec[0], deg, pix)
+    plt.scatter(xmmra, xmmdec, marker='x', s=100, c='black', label=f'XMM {title}')
+    plt.title(f'{survey} image, XMM coords: {title}')
+    #plt.xlabel(f'Pixels, {deg:.2e} degrees across')
+    #plt.ylabel(f'Pixels, {deg:.2e} degrees across')
+    plt.legend()
+
+    #Axes
+    ticks=np.arange(0,pix+50, 50)
+    raticks=eq_coords(ticks, 0, ra[0], dec[0], deg, pix, 'ra')
+    decticks=eq_coords(0, ticks, ra[0], dec[0], deg, pix, 'dec')
+
+    raticks_formatted = [f'{coord:.4f}' for coord in raticks]
+    decticks_formatted = [f'{coord:.4f}' for coord in decticks]
+
+    plt.gca().set_xticks(ticks) 
+    plt.gca().set_yticks(ticks)
+    plt.gca().set_xticklabels(raticks_formatted)
+    plt.gca().set_yticklabels(decticks_formatted)
+
+
+    plt.xlabel('RA (degrees)')
+    plt.ylabel('Dec (degrees)')
+    plt.show(block=False)
+
+#Group the data
+groupedtwos = twos.groupby(['ra', 'dec'])
+groupedthrees = threes.groupby(['ra', 'dec'])
+
+#Loop over and print
+for group_name, group_data in groupedtwos:
+    get_image(np.array(group_data['specObjAll_ra']), np.array(group_data['specObjAll_dec']), 1/60, 300, group_name)
+
+for group_name, group_data in groupedthrees:
+    get_image(np.array(group_data['specObjAll_ra']), np.array(group_data['specObjAll_dec']), 1/60, 300, group_name)
+
+
+plt.show()
